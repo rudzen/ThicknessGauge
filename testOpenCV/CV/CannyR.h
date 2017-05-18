19a690897b3ce86b6b28576e5970b229b0ca8bd7
Rudy Kohn <rudzen@gmail.com>
2017-05-18 21:09:41 +0200

    Added optional peppernoise removal to CannyR + fixed member names

--- testOpenCV/CV/CannyR.h
+++ testOpenCV/CV/CannyR.h
@@ -25,28 +25,31 @@
 
 class CannyR : public BaseR<float> {
 	
-	cv::Mat edges;
+	cv::Mat edges_;
 
-	Pixelz p;
+	Pixelz pixelz_;
 
-	int threshold1;
+	int threshold1_;
 
-	int threshold2;
+	int threshold2_;
 
-	const int aperMin = 3;
-	const int aperMax = 7;
-	int apertureSize;
+	const int APER_MIN = 3;
+	const int APER_MAX = 7;
 
-	int gradient;
+	int apertureSize_;
 
-	bool showWindow;
+	int gradient_;
+
+	bool showWindow_;
+
+	bool removePepperNoise_;
 
 	void createWindow() {
 		namedWindow(windowName, cv::WINDOW_KEEPRATIO);
-		cv::createTrackbar("threshold1", windowName, &threshold1, 200, threshold1cb, this);
-		cv::createTrackbar("threshold2", windowName, &threshold2, 200, threshold2cb, this);
-		cv::createTrackbar("apertureSize", windowName, &apertureSize, aperMax, apertureSizecb, this);
-		cv::createTrackbar("gradient", windowName, &gradient, 1, gradientcb, this);
+		cv::createTrackbar("threshold1", windowName, &threshold1_, 200, threshold1cb, this);
+		cv::createTrackbar("threshold2", windowName, &threshold2_, 200, threshold2cb, this);
+		cv::createTrackbar("apertureSize", windowName, &apertureSize_, APER_MAX, apertureSizecb, this);
+		cv::createTrackbar("gradient", windowName, &gradient_, 1, gradientcb, this);
 	}
 
 	static void threshold1cb(int value, void *userData);
@@ -55,35 +58,36 @@ class CannyR : public BaseR<float> {
 	static void gradientcb(int value, void* userData);
 
 	void setThreshold1(int threshold1) {
-		this->threshold1 = threshold1;
+		this->threshold1_ = threshold1;
 	}
 
 	void setThreshold2(int threshold2) {
-		this->threshold2 = threshold2;
+		this->threshold2_ = threshold2;
 	}
 
 	void setApertureSize(int apertureSize) {
 		auto newSize = apertureSize;
-		if (newSize < aperMin)
-			newSize = aperMin;
+		if (newSize < APER_MIN)
+			newSize = APER_MIN;
 		else if (newSize % 2 == 0)
 			newSize++;
 
-		this->apertureSize = newSize;
+		this->apertureSize_ = newSize;
 		//this->apertureSize = 2 * apertureSize + 1;
 	}
 
 	void setGradient(int gradient) {
-		this->gradient = gradient;
+		this->gradient_ = gradient;
 	}
 
 public:
-	CannyR(const int threshold1, const int threshold2, const int apertureSize, const bool gradient, const bool showWindow)
-		: threshold1(threshold1),
-		  threshold2(threshold2),
-		  apertureSize(apertureSize),
-		  gradient(gradient),
-		  showWindow(showWindow) {
+	CannyR(const int threshold1, const int threshold2, const int apertureSize, const bool gradient, const bool showWindow, const bool removePepperNoise)
+		: threshold1_(threshold1),
+		  threshold2_(threshold2),
+		  apertureSize_(apertureSize),
+		  gradient_(gradient),
+		  showWindow_(showWindow),
+		  removePepperNoise_(removePepperNoise) {
 		windowName = "Canny";
 		if (showWindow)
 			createWindow();
@@ -92,7 +96,7 @@ public:
 	void doCanny();
 
 	cv::Mat& getResult() {
-		return edges;
+		return edges_;
 	}
 
 };
@@ -124,10 +128,11 @@ inline void CannyR::gradientcb(int value, void* userData) {
 
 inline void CannyR::doCanny() {
 
-	Canny(image_, edges, threshold1, threshold2, apertureSize, gradient > 0);
+	Canny(image_, edges_, threshold1_, threshold2_, apertureSize_, gradient_ > 0);
 
-	p.removePepperNoise(image_);
+	if (removePepperNoise_)
+		pixelz_.removePepperNoise(image_);
 
-	if (showWindow)
-		imshow(windowName, edges);
+	if (showWindow_)
+		imshow(windowName, edges_);
 }
