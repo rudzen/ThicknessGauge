a9cb09ae258fcc30f01891cd287e4b7fdb8e4569
Rudy Kohn <rudzen@gmail.com>
2017-05-20 10:31:30 +0200

    Slight speed increase for intersection calc

--- testOpenCV/CV/LaserR.h
+++ testOpenCV/CV/LaserR.h
@@ -128,12 +128,13 @@ inline void LaserR::configureXLine(cv::Mat& image, vector<cv::Point2i>& nonZeroe
 #ifdef CV_VERSION
 
 inline bool LaserR::intersection(cv::Point2f o1, cv::Point2f p1, cv::Point2f o2, cv::Point2f p2, cv::Point2f& r) const {
-	auto x = o2 - o1;
 	auto d1 = p1 - o1;
 	auto d2 = p2 - o2;
 
 	auto cross = d1.x * d2.y - d1.y * d2.x;
 	if (abs(cross) < /*EPS*/1e-8) return false;
+
+	auto x = o2 - o1;
 
 	double t1 = (x.x * d2.y - x.y * d2.x) / cross;
 	r = o1 + d1 * t1;
