2a86a685d4606b7171e1d2265ce340bc9e054af5
Rudy Kohn <rudzen@gmail.com>
2017-06-13 14:23:28 +0200

    formattet Main class

--- testOpenCV/Main.cpp
+++ testOpenCV/Main.cpp
@@ -1,4 +1,3 @@
-
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include <VimbaCPP/Include/VimbaSystem.h>
@@ -53,220 +52,214 @@ bool parseArgs(int argc, char** argv, CommandLineOptions& options);
 
 void saveNull(std::string filename) {
 
-	// quick and dirty hack to save null image quickly
+    // quick and dirty hack to save null image quickly
     log_time << cv::format("Enter delay in seconds before capture to %s\n>", filename);
-	int t;
-	cin >> t;
-	cv::setNumThreads(2);
-	cv::Mat nullImage;
-	cv::VideoCapture cap;
-	cap.open(CV_CAP_PVAPI);
-	cap >> nullImage;
-	cv::imwrite(filename, nullImage);
-	cap.release();
+    int t;
+    cin >> t;
+    cv::setNumThreads(2);
+    cv::Mat nullImage;
+    cv::VideoCapture cap;
+    cap.open(CV_CAP_PVAPI);
+    cap >> nullImage;
+    cv::imwrite(filename, nullImage);
+    cap.release();
 }
 
 int main(int argc, char** argv) {
 
-	auto return_value = false;
-	CommandLineOptions options;
+    auto return_value = false;
+    CommandLineOptions options;
 
-	try {
+    try {
 
-		if (parseArgs(argc, argv, options)) {
-			// unique case for build information
-			if (options.isBuildInfoMode()) {
-				log_time << cv::getBuildInformation();
-				return 0;
-			}
-			// null save mode..
-			saveNull(options.getCameraFile());
-			return 0;
-		}
+        if (parseArgs(argc, argv, options)) {
+            // unique case for build information
+            if (options.isBuildInfoMode()) {
+                log_time << cv::getBuildInformation();
+                return 0;
+            }
+            // null save mode..
+            saveNull(options.getCameraFile());
+            return 0;
+        }
 
-		auto thicknessGauge = std::make_unique<ThicknessGauge>(options.getFrames(), options.isShowWindows(), options.isRecordVideo(), 100, 100);
+        auto thicknessGauge = std::make_unique<ThicknessGauge>(options.getFrames(), options.isShowWindows(), options.isRecordVideo(), 100, 100);
 
-		//thicknessGauge->setFrameCount(options.getFrames());
-		//thicknessGauge->setShowWindows(options.isShowWindows());
-		//thicknessGauge->setSaveVideo(options.isRecordVideo());
-		thicknessGauge->initCalibrationSettings(options.getCameraFile());
-		cv::setNumThreads(options.getNumOpenCvThreads());
+        //thicknessGauge->setFrameCount(options.getFrames());
+        //thicknessGauge->setShowWindows(options.isShowWindows());
+        //thicknessGauge->setSaveVideo(options.isRecordVideo());
+        thicknessGauge->initCalibrationSettings(options.getCameraFile());
+        cv::setNumThreads(options.getNumOpenCvThreads());
 
-		//log_time << options << endl;
+        //log_time << options << endl;
 
-		if (options.isGlobMode()) {
-			// TODO : use capture for file reading?!
-			thicknessGauge->initVideoCapture();
-			auto globName = options.getGlobFolder();
-			thicknessGauge->generateGlob(globName);
-		}
-		else if (options.isDemoMode()) {// && !options.TestMode() && !options.CalibrationMode()) {
+        if (options.isGlobMode()) {
+            // TODO : use capture for file reading?!
+            thicknessGauge->initVideoCapture();
+            auto globName = options.getGlobFolder();
+            thicknessGauge->generateGlob(globName);
+        } else if (options.isDemoMode()) {// && !options.TestMode() && !options.CalibrationMode()) {
 
-			auto glob_name = options.getGlobFolder();
+            auto glob_name = options.getGlobFolder();
 
-			thicknessGauge->initialize(glob_name);
+            thicknessGauge->initialize(glob_name);
 
-			thicknessGauge->computeMarkingHeight();
+            thicknessGauge->computeMarkingHeight();
 
-			auto data = thicknessGauge->getData(); // virker :-)
+            auto data = thicknessGauge->getData(); // virker :-)
 
-			thicknessGauge->saveData("output_mufmuf");
+            thicknessGauge->saveData("output_mufmuf");
 
-			log_timedate << cv::format("difference: %f\n", data->difference);
+            log_timedate << cv::format("difference: %f\n", data->difference);
 
-			//auto result = c.findMarkingLinePairs_(globName);
-			//log_time << result << endl;
-			//returnValue = result.first.x != 0;
-			//returnValue = c.generatePlanarImage(globName);
-			//if (returnValue) {
-			log_time << "done..\n";
-			//}
-		}
-		else if (options.isCalibrationMode()) {
-			throw CalibrationException("Unable to initiate calibration mode, feature not completed.");
-		}
-		else if (options.isTestMode()) {
-			//c.initVideoCapture();
-			//c.testAggressive();
-		}
-	}
-	catch (ArgException& ae) {
-		string what = ae.what();
-		log_timedate << "Exception suddenly happend (but what?)\n" + what << std::endl;
-		return -1;
-	}
-	catch (CaptureFailException& cfe) {
-		string what = cfe.what();
-		log_timedate << "Exception suddenly happend (but what?)\n" + what << std::endl;
-		return -2;
-	}
-	catch (CalibrationException& cale) {
-		string what = cale.what();
-		log_timedate << "Exception suddenly happend (but what?)\n" + what << std::endl;
-		return -3;
-	}
-	catch (TestException& te) {
-		string what = te.what();
-		log_timedate << "Exception suddenly happend (but what?)\n" + what << std::endl;
-		return -4;
-	}
-	log_time << cv::format("Smooth operator >> %i\n", return_value);
+            //auto result = c.findMarkingLinePairs_(globName);
+            //log_time << result << endl;
+            //returnValue = result.first.x != 0;
+            //returnValue = c.generatePlanarImage(globName);
+            //if (returnValue) {
+            log_time << "done..\n";
+            //}
+        } else if (options.isCalibrationMode()) {
+            throw CalibrationException("Unable to initiate calibration mode, feature not completed.");
+        } else if (options.isTestMode()) {
+            //c.initVideoCapture();
+            //c.testAggressive();
+        }
+    } catch (ArgException& ae) {
+        string what = ae.what();
+        log_timedate << "Exception suddenly happend (but what?)\n" + what << std::endl;
+        return -1;
+    }
+    catch (CaptureFailException& cfe) {
+        string what = cfe.what();
+        log_timedate << "Exception suddenly happend (but what?)\n" + what << std::endl;
+        return -2;
+    }
+    catch (CalibrationException& cale) {
+        string what = cale.what();
+        log_timedate << "Exception suddenly happend (but what?)\n" + what << std::endl;
+        return -3;
+    }
+    catch (TestException& te) {
+        string what = te.what();
+        log_timedate << "Exception suddenly happend (but what?)\n" + what << std::endl;
+        return -4;
+    }
+    log_time << cv::format("Smooth operator >> %i\n", return_value);
 
-	return 0;
+    return 0;
 
 }
 
 bool parseArgs(int argc, char** argv, CommandLineOptions& options) {
-	try {
-		CmdLine cmd("ThicknessGauge [OpenCV]", '=', "0.1", true);
+    try {
+        CmdLine cmd("ThicknessGauge [OpenCV]", '=', "0.1", true);
 
-		/* begin singular switches, only one permitted, default is demo mode */
+        /* begin singular switches, only one permitted, default is demo mode */
 
-		// xor args
-		vector<Arg*> xors;
+        // xor args
+        vector<Arg*> xors;
 
-		// add basic switches
-		SwitchArg switch_demo("d", "demo", "runs regular demo", true, new DemoModeVisitor());
-		xors.emplace_back(&switch_demo);
-		//cmd.add(demoSwitch);
+        // add basic switches
+        SwitchArg switch_demo("d", "demo", "runs regular demo", true, new DemoModeVisitor());
+        xors.emplace_back(&switch_demo);
+        //cmd.add(demoSwitch);
 
-		SwitchArg switch_calibration("c", "calibrate", "perform camera calibration", false, new CalibrationModeVisitor());
-		xors.emplace_back(&switch_calibration);
-		//cmd.add(calibrationSwitch);
+        SwitchArg switch_calibration("c", "calibrate", "perform camera calibration", false, new CalibrationModeVisitor());
+        xors.emplace_back(&switch_calibration);
+        //cmd.add(calibrationSwitch);
 
-		SwitchArg switch_buildinfo("i", "info", "show software information", false, new BuildInfoVisitor());
-		xors.emplace_back(&switch_buildinfo);
-		//cmd.add(buildInfoSwitch);
+        SwitchArg switch_buildinfo("i", "info", "show software information", false, new BuildInfoVisitor());
+        xors.emplace_back(&switch_buildinfo);
+        //cmd.add(buildInfoSwitch);
 
-		SwitchArg switch_test("t", "test", "Perform test", false, new TestModeVisitor());
-		xors.emplace_back(&switch_test);
+        SwitchArg switch_test("t", "test", "Perform test", false, new TestModeVisitor());
+        xors.emplace_back(&switch_test);
 
-		SwitchArg switch_glob("g", "save_glob", "Save -f frames as glob", false, new GlobModeVisitor());
-		xors.emplace_back(&switch_glob);
+        SwitchArg switch_glob("g", "save_glob", "Save -f frames as glob", false, new GlobModeVisitor());
+        xors.emplace_back(&switch_glob);
 
-		cmd.xorAdd(xors);
+        cmd.xorAdd(xors);
 
-		ValueArg<string> arg_nullsave("", "null_save", "Save a singular image and exit", false, "null.png", new TestSuitConstraint());
-		cmd.add(arg_nullsave);
+        ValueArg<string> arg_nullsave("", "null_save", "Save a singular image and exit", false, "null.png", new TestSuitConstraint());
+        cmd.add(arg_nullsave);
 
-		/* end switches */
+        /* end switches */
 
-		/* begin value base argument*/
-		ValueArg<string> arg_testsuite("", "test_suite", "Test name for saving the test under.", false, "default", new TestSuitConstraint());
-		cmd.add(arg_testsuite);
+        /* begin value base argument*/
+        ValueArg<string> arg_testsuite("", "test_suite", "Test name for saving the test under.", false, "default", new TestSuitConstraint());
+        cmd.add(arg_testsuite);
 
-		ValueArg<int> arg_frame("", "frames", "amount of frames each calculation", false, 25, new IntegerConstraint("Frames", 5, 200));
-		cmd.add(arg_frame);
+        ValueArg<int> arg_frame("", "frames", "amount of frames each calculation", false, 25, new IntegerConstraint("Frames", 5, 200));
+        cmd.add(arg_frame);
 
-		ValueArg<bool> arg_show_windows("", "show_windows", "displays windows in demo mode", false, true, "0/1");
-		cmd.add(arg_show_windows);
+        ValueArg<bool> arg_show_windows("", "show_windows", "displays windows in demo mode", false, true, "0/1");
+        cmd.add(arg_show_windows);
 
-		ValueArg<bool> arg_record_video("", "record_video", "Records demo mode to video", false, false, "0/1");
-		cmd.add(arg_record_video);
+        ValueArg<bool> arg_record_video("", "record_video", "Records demo mode to video", false, false, "0/1");
+        cmd.add(arg_record_video);
 
-		ValueArg<string> arg_camera_calibration_file("", "camera_settings", "OpenCV camera calibration file", false, default_camera_calibration_file, new FileConstraint());
-		cmd.add(arg_camera_calibration_file);
+        ValueArg<string> arg_camera_calibration_file("", "camera_settings", "OpenCV camera calibration file", false, default_camera_calibration_file, new FileConstraint());
+        cmd.add(arg_camera_calibration_file);
 
-		ValueArg<string> arg_calibration_output("", "calibrate_output", "output file for camera matricies", false, "output.json", "filename");
-		cmd.add(arg_calibration_output);
+        ValueArg<string> arg_calibration_output("", "calibrate_output", "output file for camera matricies", false, "output.json", "filename");
+        cmd.add(arg_calibration_output);
 
-		ValueArg<int> arg_max_opencv_threads("", "opencv_threads", "OpenCV thread limit", false, 4, new IntegerConstraint("OpenCV Threads", 1, 40));
-		cmd.add(arg_max_opencv_threads);
+        ValueArg<int> arg_max_opencv_threads("", "opencv_threads", "OpenCV thread limit", false, 4, new IntegerConstraint("OpenCV Threads", 1, 40));
+        cmd.add(arg_max_opencv_threads);
 
-		ValueArg<string> arg_glob_name("", "glob_name", "Name to save glob as", false, "camera", "Valid folder name.");
-		cmd.add(arg_glob_name);
+        ValueArg<string> arg_glob_name("", "glob_name", "Name to save glob as", false, "camera", "Valid folder name.");
+        cmd.add(arg_glob_name);
 
-		cmd.parse(argc, argv);
+        cmd.parse(argc, argv);
 
-		// check for null save.. this is an important thing! :-)
-		if (arg_nullsave.isSet()) {
-			options.setCameraFile(arg_nullsave.getValue());
-			return true;
-		}
+        // check for null save.. this is an important thing! :-)
+        if (arg_nullsave.isSet()) {
+            options.setCameraFile(arg_nullsave.getValue());
+            return true;
+        }
 
-		// read all parsed command line arguments
-		if (switch_demo.isSet())
-			options.setDemoMode(true);
-		else if (switch_calibration.isSet())
-			options.setCalibrationMode(true);
-		else if (switch_test.isSet())
-			options.setTestMode(true);
-		else if (switch_buildinfo.isSet()) { // check, its a instant abort if build info is found
-			options.setBuildInfoMode(true);
-			return true;
-		}
-		else if (switch_glob.isSet())
-			options.setGlobMode(true); // glob mode
+        // read all parsed command line arguments
+        if (switch_demo.isSet())
+            options.setDemoMode(true);
+        else if (switch_calibration.isSet())
+            options.setCalibrationMode(true);
+        else if (switch_test.isSet())
+            options.setTestMode(true);
+        else if (switch_buildinfo.isSet()) { // check, its a instant abort if build info is found
+            options.setBuildInfoMode(true);
+            return true;
+        } else if (switch_glob.isSet())
+            options.setGlobMode(true); // glob mode
 
-		auto sval = arg_camera_calibration_file.getValue();
-		options.setCameraFile(sval);
+        auto sval = arg_camera_calibration_file.getValue();
+        options.setCameraFile(sval);
 
-		sval = arg_calibration_output.getValue();
-		options.setCalibrationOutput(sval);
+        sval = arg_calibration_output.getValue();
+        options.setCalibrationOutput(sval);
 
-		sval = arg_testsuite.getValue();
-		options.setTestSuite(sval);
+        sval = arg_testsuite.getValue();
+        options.setTestSuite(sval);
 
-		sval = arg_glob_name.getValue();
-		options.setGlobFolder(sval);
+        sval = arg_glob_name.getValue();
+        options.setGlobFolder(sval);
 
-		auto ival = arg_frame.getValue();
-		options.setFrames(ival);
+        auto ival = arg_frame.getValue();
+        options.setFrames(ival);
 
-		ival = arg_max_opencv_threads.getValue();
-		options.setNumOpenCvThreads(ival);
+        ival = arg_max_opencv_threads.getValue();
+        options.setNumOpenCvThreads(ival);
 
-		auto bval = arg_show_windows.getValue();
-		options.setShowWindows(bval);
+        auto bval = arg_show_windows.getValue();
+        options.setShowWindows(bval);
 
-		bval = arg_record_video.getValue();
-		options.setRecordVideo(bval);
+        bval = arg_record_video.getValue();
+        options.setRecordVideo(bval);
 
-		return false;
-	}
-	catch (ArgException& e) {
-		throw(e);
-	}
+        return false;
+    } catch (ArgException& e) {
+        throw(e);
+    }
 }
 
 //void test_video(cv::Mat input) {
