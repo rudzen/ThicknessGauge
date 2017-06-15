e1882fff183f9deb3a6c5d4e92d39ed30cb90efb
Rudy Kohn <rudzen@gmail.com>
2017-06-15 13:26:49 +0200

    Removed old crap from main class

--- testOpenCV/Main.cpp
+++ testOpenCV/Main.cpp
@@ -91,7 +91,7 @@ int main(int argc, char** argv) {
         thicknessGauge->initCalibrationSettings(options.getCameraFile());
         cv::setNumThreads(options.getNumOpenCvThreads());
 
-        log_time << cv::format("OpenCV Optimization use %b\n", cv::useOptimized());
+        log_time << cv::format("OpenCV Optimization use : %i\n", cv::useOptimized());
 
         //log_time << options << endl;
 
@@ -257,95 +257,3 @@ bool parseArgs(int argc, char** argv, CommandLineOptions& options) {
         throw(e);
     }
 }
-
-//void test_video(cv::Mat input) {
-//
-//	// Lambda Matrix
-//	Mat lambda(2, 4, CV_32FC1);
-//	//Input and Output Image;
-//	Mat output;
-//
-//	// Set the lambda matrix the same type and size as input
-//	lambda = Mat::zeros(input.rows, input.cols, input.type());
-//
-//	// The 4 points that select quadilateral on the input , from top-left in clockwise order
-//	// These four pts are the sides of the rect box used as input 
-//	// Input Quadilateral or Image plane coordinates
-//	Point2f inputQuad[4];
-//	CV::GenerateInputQuad(&input, inputQuad);
-//
-//	// Output Quadilateral or World plane coordinates
-//	Point2f outputQuad[4];
-//	CV::GenerateOutputQuad(&input, outputQuad);
-//
-//	CV::drawCenterAxisLines(&input);
-//
-//	Mat diffImage, doubleDiffImage;
-//	Mat outputInverted;
-//
-//	// configure ImageSave for video output.
-//	ImageSave is;
-//	is.SetInformation(Information::Full);
-//	is.SetSaveType(SaveType::Video);
-//	is.SetCodec(VideoCodec::Mjpeg);
-//	is.SetFPS(1.0f);
-//	is.SetSize(input.cols, input.rows);
-//	is.SetColour(VideoColour::Colour);
-//	is.SetFileName("Test_numero_uno_pelikan");
-//	is.OpenVideo();
-//
-//	CV::FitQuad(&input, inputQuad, outputQuad);
-//
-//	float ff;
-//
-//	const int max = 360;
-//
-//	int q1 = 0;
-//
-//	for (int i = 0; i < max; ++i) {
-//		ff = static_cast<float>(i);
-//		//inputQuad[0] = Point2f(-30.0f, -60.0f);
-//		//inputQuad[1] = Point2f(input.cols + 50.0f, -50.0f);
-//		//inputQuad[2] = Point2f(input.cols + 100.0f, input.rows + 250.0f);
-//		//inputQuad[3] = Point2f(-50.0f, input.rows + 50.0f);
-//
-//		//// The 4 points where the mapping is to be done , from top-left in clockwise order
-//		//outputQuad[0] = Point2f(0, 0);
-//		//outputQuad[1] = Point2f(input.cols - 1.0f, 0.0f);
-//		//outputQuad[2] = Point2f(input.cols - 1.0f, input.rows - 1.0f);
-//		//outputQuad[3] = Point2f(0, input.rows - 1.0f);
-//
-//		// Get the Perspective Transform Matrix i.e. lambda 
-//		lambda = getPerspectiveTransform(outputQuad, inputQuad);
-//
-//		WRITELOG(logger, framework::Diagnostics::LogLevel::Info, _T("Warping perspective."));
-//
-//		// Apply the Perspective Transform just found to the src image
-//		warpPerspective(input, output, lambda, output.size(), INTER_CUBIC);
-//
-//		WRITELOG(logger, framework::Diagnostics::LogLevel::Info, _T("Reversing warp."));
-//
-//		// invert lambda and "recreate" original image
-//		warpPerspective(output, outputInverted, lambda.inv(), outputInverted.size(), INTER_LANCZOS4);
-//
-//		WRITELOG(logger, framework::Diagnostics::LogLevel::Info, _T("Finding leftover artifacts."));
-//
-//		// get differences between original and restored original image and resize it
-//		absdiff(input, outputInverted, diffImage);
-//		pyrUp(diffImage, doubleDiffImage);
-//		//resize(diffImage, doubleDiffImage, Size(), 2.0f, 2.0f, INTER_CUBIC);
-//
-//		is.SaveVideoFrame(output);
-//
-//		//is.UpdateTimeStamp();
-//		//is.SaveImage(&output, "OutputTest " + std::to_string(i));
-//
-//		ff = (static_cast<float>(i) / max) * 100;
-//		log_time << "                                                            " << '\xd';
-//		log_time << '\xd' << ff << "%";
-//
-//	}
-//
-//	is.CloseVideo();
-//
-//}
